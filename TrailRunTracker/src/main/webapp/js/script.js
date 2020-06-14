6a08bcc370bfa3172b1af489cc0c90aae37dcacd
Rich Wasek <rideburtonrw@gmail.com>
2020-06-13 18:04:02 -0600

    Updated JS Post method to create new run event

--- TrailRunTracker/src/main/webapp/js/script.js
+++ TrailRunTracker/src/main/webapp/js/script.js
@@ -6,7 +6,14 @@ function init() {
   console.log("script.js loaded");
   getEvents(); // store this in a variable or use the one from line 20?
   createTable();
+
+  document.createRunForm.addTrailRun.addEventListener('click', function(e){
+	e.preventDefault();
+	createNewRun();
+});
 }
+
+// Method to get all the Events and display them on the homepage:
 
 function getEvents() {
   let xhr = new XMLHttpRequest();
@@ -36,12 +43,13 @@ function createTable(eventArray) {
   runTableDiv.appendChild(table);
   let tableHead = createTableHead();
   table.appendChild(tableHead);
-  for (let i = 0; i < eventArray.length; i++) {
-	  let individualRun = eventArray[i];
-	  let tableRow = createRow(individualRun);
-	  table.appendChild(tableRow);
+  if (typeof eventArray !== 'undefined') {
+	for (let i = 0; i < eventArray.length; i++) {
+		let individualRun = eventArray[i];
+		let tableRow = createRow(individualRun);
+		table.appendChild(tableRow);
+	}
   }
-  
 }
 
 function createTableHead() {
@@ -59,7 +67,7 @@ function createTableHead() {
 }
 
 function createRow(individualRun) {
-		
+
 		let tr = document.createElement("tr");
 
         let tdName = document.createElement("td");
@@ -72,5 +80,65 @@ function createRow(individualRun) {
         tr.appendChild(tdName);
         tr.appendChild(tdLocation);
 		tr.appendChild(tdDistance);
+
+		tr.addEventListener('click', function(e){
+			e.preventDefault();
+			displayIndividualRun(individualRun);
+		});
 		return tr;
+}
+
+function displayIndividualRun(individualRun){
+	let individualRunDiv = document.getElementById('trailRunDetails');
+	individualRunDiv.textContent = 'You clicked?';
+}
+
+// Create new  methods:
+
+function createNewRun() {
+
+	
+	let form = document.createRunForm;
+	let trailRun = {};
+	trailRun.trailName = form.trailName.value;
+	trailRun.location = form.location.value;
+	trailRun.date = form.date.value;
+	trailRun.totalTime = form.totalTime.value;
+	trailRun.distance = form.distance.value;
+	trailRun.averagePace = form.averagePace.value;
+	trailRun.bestPace = form.bestPace.value;
+	trailRun.elevationGain = form.elevationGain.value;
+	trailRun.maxHeartRate = form.maxHeartRate.value;
+	trailRun.avgHeartRate = form.avgHeartRate.value;
+	trailRun.description = form.description.value;
+	trailRun.trailType = form.trailType.value;
+	trailRun.active = true;
+	console.log(trailRun);
+	postNewRun(trailRun);
+} 
+
+function postNewRun(trailRun) {
+	let trailRunJson = JSON.stringify(trailRun);
+	let xhr = new XMLHttpRequest();
+	let uri = 'api/trailruns';
+	xhr.open('POST', uri);
+	xhr.setRequestHeader('Content-type', 'application/json');
+	xhr.onreadystatechange = function() {
+		if (xhr.readyState === 4) {
+			if (xhr.status === 200 || xhr.status === 201) {
+				let createdTrailRun = JSON.parse(xhr.responseText);
+				location.reload();
+			}
+			else {
+				if (xhr.status === 400) {
+					// create an error div
+					console.log('Invalid film data')
+				}
+				else {
+					console.log('Unknown error creating film' + xhr.status);
+				}
+			}
+		}
+	};
+	xhr.send(trailRunJson);
 }
